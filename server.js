1018052dee64e918439e64325137d3b4a36c3445
eduardoc <eduardopsy1@gmail.com>
2025-05-19 23:32:06 -0600

    chore: servidor Express listo y app funcionando localmente

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// server.js
-import express from "express";
-import path from "path";
+// server.js (CommonJS)
+const express = require("express");
+const path    = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Sirve todos los archivos estáticos (index.html, CSS, JS, imágenes)
-app.use(express.static(path.resolve("./")));
+app.use(express.static(path.resolve(__dirname, "./")));
 
-// En cualquier otra ruta, devuelve index.html (para que funcione tu SPA)
+// Para cualquier otra ruta, devuelve index.html (SPA)
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve("index.html"));
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
